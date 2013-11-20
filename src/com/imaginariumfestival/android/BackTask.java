3f2f84e1fdf961c4f41ca445e9b2506e31d38b35
Martin Salles <salles.martin@gmail.com>
2013-11-20 17:36:31 -0500

    webservices are used properly, data is updated only if the table has changed on the remote server

--- src/com/imaginariumfestival/android/BackTask.java
+++ src/com/imaginariumfestival/android/BackTask.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.http.HttpEntity;
@@ -51,7 +52,7 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 	@Override
 	protected void onPreExecute() {
 		super.onPreExecute();
-		Toast.makeText(context, "Mise à jour des données...", Toast.LENGTH_SHORT)
+		Toast.makeText(context, "Mise à jour des données...", Toast.LENGTH_LONG)
 				.show();
 	}
 
@@ -75,7 +76,7 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 	
 	private void getArtistsFromWebService() {
 		final SharedPreferences pref = context.getSharedPreferences(BackTask.LAST_ARTIST_UPDATE_FROM_DISTANT_DATABASE, Context.MODE_PRIVATE);
-        final long lastRetrieve = pref.getLong(BackTask.LAST_ARTIST_UPDATE_FROM_DISTANT_DATABASE, 0L);
+        final String lastRetrieve = pref.getString(BackTask.LAST_ARTIST_UPDATE_FROM_DISTANT_DATABASE, "");
 		final String url = ARTISTS_WEB_SERVICE_URL + "&lastRetrieve=" + lastRetrieve;
 		String artistsFromDistantDatabase = getDataFromDistantDatabase(url);
 		
@@ -91,7 +92,6 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 			updateLastUpdateFromDatabase(LAST_ARTIST_UPDATE_FROM_DISTANT_DATABASE);
 		}
 	}
-
 	
 	private Boolean recordArtists(JSONArray jsonArtistsArray) {
 		ArtistDataSource datasource = new ArtistDataSource(context);
@@ -130,7 +130,7 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 	
 	private void getInfosFromWebService() {
 		final SharedPreferences pref = context.getSharedPreferences(BackTask.LAST_INFO_UPDATE_FROM_DISTANT_DATABASE, Context.MODE_PRIVATE);
-        final long lastRetrieve = pref.getLong(BackTask.LAST_INFO_UPDATE_FROM_DISTANT_DATABASE, 0L);
+        final String lastRetrieve = pref.getString(BackTask.LAST_INFO_UPDATE_FROM_DISTANT_DATABASE, "");
 		final String url = INFOS_WEB_SERVICE_URL + "&lastRetrieve=" + lastRetrieve;
 		String infosFromDistantDatabase = getDataFromDistantDatabase(url);
 		
@@ -174,7 +174,7 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 	
 	private void getNewsFromWebService() {
 		final SharedPreferences pref = context.getSharedPreferences(BackTask.LAST_NEWS_UPDATE_FROM_DISTANT_DATABASE, Context.MODE_PRIVATE);
-        final long lastRetrieve = pref.getLong(BackTask.LAST_NEWS_UPDATE_FROM_DISTANT_DATABASE, 0L);
+        final String lastRetrieve = pref.getString(BackTask.LAST_NEWS_UPDATE_FROM_DISTANT_DATABASE, "");
 		final String url = NEWS_WEB_SERVICE_URL + "&lastRetrieve=" + lastRetrieve;
 		String newsFromDistantDatabase = getDataFromDistantDatabase(url);
 		
@@ -190,7 +190,6 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 			updateLastUpdateFromDatabase(LAST_NEWS_UPDATE_FROM_DISTANT_DATABASE);
 		}
 	}
-
 	
 	private Boolean recordNews(JSONArray jsonNewsArray) {
 		NewsDataSource datasource = new NewsDataSource(context);
@@ -248,10 +247,12 @@ class BackTask extends AsyncTask<Void, Integer, Void> {
 	
 	private void updateLastUpdateFromDatabase(final String prefToUpdate) {
 		Date currentDate = new Date();
-
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd%20hh:mm:ss");
+		String formattedDate = dateFormat.format(currentDate);
+		
 		SharedPreferences pref = context.getSharedPreferences(prefToUpdate, Context.MODE_PRIVATE);
 		Editor editor = pref.edit();
-		editor.putLong(prefToUpdate, currentDate.getTime());
+		editor.putString(prefToUpdate, formattedDate);
 		editor.commit();
 	}
 }
